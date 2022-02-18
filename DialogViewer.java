d03a112912d126880b4643dbcb698a86bd481ce9
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2022-02-18 09:57:29 +0100

    Add files via upload

---
+++ DialogViewer.java
@@ -0,0 +1,38 @@
+/*
+Esercizio P1.7
+Copiate ed eseguite il programma seguente:
+
+import javax.swing.JOptionPane;
+
+public class DialogViewer
+{
+
+    public static void main(String[] args)
+    {
+
+        JOptionPane.showMessageDialog(null, "Hello, World!");
+
+        System.exit(0);
+
+    }
+
+}
+
+Modificate poi il programma in modo che visualizzi  il messaggio "Hello, vostro nome!".
+*/
+
+import javax.swing.JOptionPane;
+
+public class DialogViewer
+{
+
+    public static void main(String[] args)
+    {
+
+        JOptionPane.showMessageDialog(null, "Hello, Vincenzo!");
+
+        System.exit(0);
+
+    }
+
+}
