58cd143e6e5e707aa8ef87a4f8d0522010e3fa76
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2022-02-18 10:55:11 +0100

    Create Hello.java

---
+++ 1 - Hello/Hello.java
@@ -0,0 +1,24 @@
+public class Hello
+{
+
+  public static void main(String[] args)
+  {
+
+    System.out.println("Hello, World!");
+
+    System.exit(0);
+
+  }
+
+}
+
+/*
+
+Il file sorgente con il codice, deve essere salvato con estensione .java
+inoltre il nome del file deve essere uguale al nome della classe del codice
+
+ad esempio public class Hello
+
+sarà salvato come Hello.java
+
+*/
