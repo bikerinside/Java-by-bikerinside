8c6fdea72614d444d2aa0facf7c0138f2c32693e
Vincenzo Rosario <vincenzor.casillo@gmail.com>
2022-02-18 09:08:59 +0100

    Update Hello.java

--- Hello/Hello.java
+++ Hello/Hello.java
@@ -11,3 +11,14 @@ public class Hello
   }
 
 }
+
+/*
+
+Il file sorgente con il codice, deve essere salvato con estensione .java
+inoltre il nome del file deve essere uguale al nome della classe del codice
+
+ad esempio public class Hello
+
+sarà salvato come Hello.java
+
+*/
